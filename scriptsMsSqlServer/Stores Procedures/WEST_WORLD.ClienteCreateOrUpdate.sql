6ca749110e7a8cc8b4324862bf470b7ad3649583
nachosoto93 <nachosoto.93>
2017-11-01 12:59:39 -0300

    Stores Procedures

---
+++ scriptsMsSqlServer/Stores Procedures/WEST_WORLD.ClienteCreateOrUpdate.sql
@@ -0,0 +1,58 @@
+﻿CREATE PROCEDURE WEST_WORLD.ClienteCreateOrUpdate
+@mode nvarchar(10),
+@idCliente bigint,
+@nombre nvarchar(255),
+@apellido nvarchar(255),
+@mail nvarchar(255),
+@direccion nvarchar(255),
+@codigoPostal nvarchar(255),
+@dni numeric(15),
+@telefono numeric(15),
+@fec_nac dateTime,
+@habilitado bit
+
+
+AS
+	IF @mode='Add'
+	BEGIN
+		INSERT INTO WEST_WORLD.Cliente(
+			nombre, 
+			apellido, 
+			mail ,
+			direccion, 
+			codigoPostal, 
+			dni, 
+			telefono, 
+			fecha_nac,
+			habilitado
+		)
+		VALUES(
+			@nombre,
+			@apellido,
+			@mail,
+			@direccion,
+			@codigoPostal,
+			@dni,
+			@telefono,
+			@fec_nac,
+			@habilitado
+		)
+	END
+	ELSE IF @mode ='Edit'
+	BEGIN
+		UPDATE WEST_WORLD.Cliente
+		SET nombre=@nombre, 
+			apellido=@apellido, 
+			mail=@mail ,
+			direccion=@direccion, 
+			codigoPostal=@codigoPostal, 
+			dni=@dni, 
+			telefono=@telefono, 
+			fecha_nac=@fec_nac,
+			habilitado=@habilitado
+		WHERE idCliente=@idCliente
+	END
+		
+		
+
+RETURN 0
---
+++ scriptsMsSqlServer/Stores Procedures/WEST_WORLD.ClienteViewOrSearch.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE WEST_WORLD.ClienteViewOrSearch
+	@nombre nvarchar(255),
+	@apellido nvarchar(255),
+	@dni numeric(15)
+AS
+	SELECT *
+	FROM WEST_WORLD.Cliente
+	WHERE nombre LIKE @nombre + '%' 
+			AND apellido LIKE @apellido + '%' 
+			 AND (@dni     IS NULL OR (DNI     = @dni    ))
+        OPTION (RECOMPILE)
+			
+RETURN 0
