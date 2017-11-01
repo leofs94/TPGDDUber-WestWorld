1bdc3e8c40e23f3230697f48dd214be62d524967
nachosoto93 <nachosoto.93>
2017-11-01 13:42:08 -0300

    empresaABM

--- PagoAgilFrba/AbmEmpresa/empresaABM.Designer.cs
+++ PagoAgilFrba/AbmEmpresa/empresaABM.Designer.cs
@@ -28,20 +28,227 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rubroComboBox = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.nombreTextBox = new System.Windows.Forms.TextBox();
+            this.cuitTextBox = new System.Windows.Forms.TextBox();
+            this.direccionTextBox = new System.Windows.Forms.TextBox();
+            this.empresaDataGrid = new System.Windows.Forms.DataGridView();
+            this.Filtrar = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cuitFilter = new System.Windows.Forms.TextBox();
+            this.nombreFilter = new System.Windows.Forms.TextBox();
+            this.rubroFilter = new System.Windows.Forms.ComboBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.empresaDataGrid)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cuit";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(334, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Direccion";
+            // 
+            // rubroComboBox
+            // 
+            this.rubroComboBox.FormattingEnabled = true;
+            this.rubroComboBox.Location = new System.Drawing.Point(399, 52);
+            this.rubroComboBox.Name = "rubroComboBox";
+            this.rubroComboBox.Size = new System.Drawing.Size(121, 21);
+            this.rubroComboBox.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(337, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Rubro";
+            // 
+            // nombreTextBox
+            // 
+            this.nombreTextBox.Location = new System.Drawing.Point(99, 12);
+            this.nombreTextBox.Name = "nombreTextBox";
+            this.nombreTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nombreTextBox.TabIndex = 5;
+            // 
+            // cuitTextBox
+            // 
+            this.cuitTextBox.Location = new System.Drawing.Point(99, 55);
+            this.cuitTextBox.Name = "cuitTextBox";
+            this.cuitTextBox.Size = new System.Drawing.Size(100, 20);
+            this.cuitTextBox.TabIndex = 6;
+            // 
+            // direccionTextBox
+            // 
+            this.direccionTextBox.Location = new System.Drawing.Point(399, 10);
+            this.direccionTextBox.Name = "direccionTextBox";
+            this.direccionTextBox.Size = new System.Drawing.Size(121, 20);
+            this.direccionTextBox.TabIndex = 7;
+            // 
+            // empresaDataGrid
+            // 
+            this.empresaDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.empresaDataGrid.Location = new System.Drawing.Point(26, 239);
+            this.empresaDataGrid.Name = "empresaDataGrid";
+            this.empresaDataGrid.Size = new System.Drawing.Size(665, 150);
+            this.empresaDataGrid.TabIndex = 8;
+            // 
+            // Filtrar
+            // 
+            this.Filtrar.AutoSize = true;
+            this.Filtrar.Location = new System.Drawing.Point(31, 128);
+            this.Filtrar.Name = "Filtrar";
+            this.Filtrar.Size = new System.Drawing.Size(32, 13);
+            this.Filtrar.TabIndex = 9;
+            this.Filtrar.Text = "Filtrar";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(88, 181);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Nombre";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(230, 181);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Cuit";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(362, 181);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(36, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Rubro";
+            // 
+            // cuitFilter
+            // 
+            this.cuitFilter.Location = new System.Drawing.Point(273, 178);
+            this.cuitFilter.Name = "cuitFilter";
+            this.cuitFilter.Size = new System.Drawing.Size(41, 20);
+            this.cuitFilter.TabIndex = 13;
+            // 
+            // nombreFilter
+            // 
+            this.nombreFilter.Location = new System.Drawing.Point(138, 178);
+            this.nombreFilter.Name = "nombreFilter";
+            this.nombreFilter.Size = new System.Drawing.Size(36, 20);
+            this.nombreFilter.TabIndex = 14;
+            // 
+            // rubroFilter
+            // 
+            this.rubroFilter.FormattingEnabled = true;
+            this.rubroFilter.Location = new System.Drawing.Point(404, 178);
+            this.rubroFilter.Name = "rubroFilter";
+            this.rubroFilter.Size = new System.Drawing.Size(121, 21);
+            this.rubroFilter.TabIndex = 15;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(616, 55);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 16;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            //this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(604, 176);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 17;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
             // 
             // empresaABM
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(992, 277);
+            this.ClientSize = new System.Drawing.Size(992, 452);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.rubroFilter);
+            this.Controls.Add(this.nombreFilter);
+            this.Controls.Add(this.cuitFilter);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Filtrar);
+            this.Controls.Add(this.empresaDataGrid);
+            this.Controls.Add(this.direccionTextBox);
+            this.Controls.Add(this.cuitTextBox);
+            this.Controls.Add(this.nombreTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.rubroComboBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "empresaABM";
             this.Text = "ABM Empresa";
-            this.Load += new System.EventHandler(this.empresaABM_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.empresaDataGrid)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox rubroComboBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox nombreTextBox;
+        private System.Windows.Forms.TextBox cuitTextBox;
+        private System.Windows.Forms.TextBox direccionTextBox;
+        private System.Windows.Forms.DataGridView empresaDataGrid;
+        private System.Windows.Forms.Label Filtrar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox cuitFilter;
+        private System.Windows.Forms.TextBox nombreFilter;
+        private System.Windows.Forms.ComboBox rubroFilter;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnBuscar;
     }
 }
--- PagoAgilFrba/AbmEmpresa/empresaABM.cs
+++ PagoAgilFrba/AbmEmpresa/empresaABM.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -11,15 +12,62 @@ using System.Windows.Forms;
 namespace PagoAgilFrba.AbmEmpresa
 {
     public partial class empresaABM : Form
+
     {
+
+        SqlConnection sqlCon = new SqlConnection(@"Data Source=10.15.241.254;Integrated Security=False;User ID=SA;Password=uD4D9uwT9hNjV7;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         public empresaABM()
         {
             InitializeComponent();
         }
 
-        private void empresaABM_Load(object sender, EventArgs e)
+        private void btnGuardar_Click(object sender, EventArgs e)
         {
+            try
+            {
+                if (sqlCon.State == ConnectionState.Closed)
+                {
+                    sqlCon.Open();
+                    if (btnGuardar.Text == "Guardar")
+                    {
+                        SqlCommand sqlCmd = new SqlCommand("GD2C2017.WEST_WORLD.EmpresaCreateOrUpdate", sqlCon);
+                        sqlCmd.CommandType = CommandType.StoredProcedure;
+                        sqlCmd.Parameters.AddWithValue("@mode", "Add");
+                        sqlCmd.Parameters.AddWithValue("@idEmpresa", 0);
+                        sqlCmd.Parameters.AddWithValue("@nombre", nombreTextBox.Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@cuit", cuitTextBox.Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@direccion", direccionTextBox.Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@rubro", rubroComboBox.TabIndex);
 
+                        sqlCmd.ExecuteNonQuery();
+                        MessageBox.Show("Cliente creado");
+                    }
+                    else
+                    {
+                        //SqlCommand sqlCmd = new SqlCommand("GD2C2017.WEST_WORLD.ClienteCreateOrUpdate", sqlCon);
+                        //sqlCmd.CommandType = CommandType.StoredProcedure;
+                        //sqlCmd.Parameters.AddWithValue("@mode", "Edit");
+                        //sqlCmd.Parameters.AddWithValue("@idCliente", Convert.ToInt32(clienteDataGrid.CurrentRow.Cells[0].Value.ToString()));
+                        //sqlCmd.Parameters.AddWithValue("@nombre", nombreTextBox.Text.Trim());
+                        //sqlCmd.Parameters.AddWithValue("@apellido", apellidoTextBox.Text.Trim());
+                        //sqlCmd.Parameters.AddWithValue("@mail", mailTextBox.Text.Trim());
+                        //sqlCmd.Parameters.AddWithValue("@direccion", direccionTextBox.Text.Trim());
+                        //sqlCmd.Parameters.AddWithValue("@codigoPostal", codigoPostalTxtBox.Text.Trim());
+                        //sqlCmd.Parameters.AddWithValue("@dni", dniTextBox.Text.Trim());
+                        //sqlCmd.Parameters.AddWithValue("@telefono", telefonoTxtBox.Text.Trim());
+                        //sqlCmd.Parameters.AddWithValue("@fec_nac", fechaNacimientoTime.Value);
+                        //sqlCmd.Parameters.AddWithValue("@habilitado", habilitadoCheck.Checked);
+                        //sqlCmd.ExecuteNonQuery();
+                        //MessageBox.Show("Cliente modificado correctamente");
+
+                    }
+
+
+                }
+            }catch(Exception ex)
+            {
+
+            }
         }
     }
 }
