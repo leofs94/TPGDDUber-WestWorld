33c36aafa79d7c4fbbe164e2d52b794625dda1e8
nachosoto93 <nachosoto.93>
2017-11-04 13:22:43 -0300

    Subo clase Const

---
+++ PagoAgilFrba/Const.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PagoAgilFrba
+{
+    static class ConnectionString
+    {
+        public const String sqlCon = @"Data Source=192.168.0.80;Integrated Security=False;User ID=SA;Password=uD4D9uwT9hNjV7;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+
+     
+
+    }
+}
